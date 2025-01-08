78cf825757d463907b1f64b40b67669330513527
farouk <bensaidf232@gmail.com>
2025-01-08 12:19:20 +0100

    Add Footer component and SlideIn animation; update layout and styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Bowlby_One_SC, DM_Mono } from "next/font/google";
 import "./globals.css";
 import { Header } from "@/components/Header";
 import { SVGFilters } from "@/components/SVGFilters";
+import { Footer } from "@/components/Footer";
 
 const bowlby = Bowlby_One_SC({
   subsets: ["latin"],
@@ -35,9 +36,10 @@ export default function RootLayout({
       >
         <main>
           <Header />
+          {children}
+          <Footer />
         </main>
         <SVGFilters />
-        {children}
       </body>
     </html>
   );
