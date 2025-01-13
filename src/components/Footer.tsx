debb9202a856b1a51125f59e79e56e59422c3db7
farouk <bensaidf232@gmail.com>
2025-01-13 12:32:04 +0100

    removing the as from the footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -37,7 +37,7 @@ export async function Footer() {
         <Logo className="pointer-events-none relative h-20 mix-blend-exclusion md:h-28" />
       </div>
 
-      <Bounded as="nav">
+      <Bounded>
         {/* List of links */}
         <ul className="flex flex-wrap justify-center gap-8 ~text-lg/xl">
           {settings.data.navigation.map((item) => (
